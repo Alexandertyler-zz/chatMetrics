83d115b341b2f26f0eea257361342d78cc5d1f16
Alex Tyler <at@norsecorp.com>
2015-12-23 09:51:17 -0800

    Made some updates to the chat listener. It now will absorb and print the login and join messages. Not sure if I want to error check this yet but I probably will want to in the future. For now I dump it to make sure logging of chat messages is done correctly. Attempts to handle this in the main irc proccessing loop were difficult and confusing so I think this solution fies both of those problems. I don't really need any of the meta data from login like the names list as all the info needed is supplied with each individual entry.

--- python/chat_controller.py
+++ python/chat_controller.py
@@ -1,7 +1,16 @@
 from chat_listener import chat_listener
 
+from pymongo import MongoClient
+
 if __name__ == '__main__':
-    chat1 = chat_listener()
+    client = MongoClient('localhost', 27017)
+    db = client.chat_logs
+
+    chat = chat_listener()
+    chat.login_routine()
+    chat.join_channel('WagamamaTV')
+    chat.
+    """chat1 = chat_listener()
     chat1.login_routine()
     chat1.join_channel('taketv')
     chat1.print_loop()
@@ -9,4 +18,6 @@ if __name__ == '__main__':
     chat2 = chat_listener()
     chat2.login_routine()
     chat2.join_channel('esl_lol')
-    chat2.print_loop()
+    chat2.print_loop()"""
+
+    
--- python/chat_listener.py
+++ python/chat_listener.py
@@ -2,10 +2,16 @@ import sys
 import socket
 import time
 
+from pymongo import MongoClient
+
 class chat_listener:
 
     def __init__(self):
         self.ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client = MongoClient('localhost', 27017)
+        self.db = self.client.chat_metrics
+        self.collection = self.db.chat_logs
+        self.log_count = 0
 
     def login_routine(self, server='irc.twitch.tv',
       port='6667', nick='ChatMetrics', 
@@ -17,33 +23,73 @@ class chat_listener:
         try:
             self.ircsock.connect((server, int(port)))
         except:
-            print('Unable to connect to %s on port %s' % server, port)
+            print('Unable to connect to %s on port %s' % (server, str(port)))
         self.ircsock.send('USER %s\r\n' % nick)
         self.ircsock.send('PASS %s\r\n' % oauth)
         self.ircsock.send('NICK %s\r\n' % nick)
+        
+        #load buffer of login and dump
+        login_msg = self.ircsock.recv(2048)
+        print login_msg
 
     def join_channel(self, channel):
         self.ircsock.send('JOIN #%s\r\n' % channel)
+        
+        #load join channel buffer and dump it
+        join_msg = self.ircsock.recv(2048)
+        print join_msg
 
     def pong(self):
-        print 'Ponging'
+        print '!!!!!!!!!!!!!!\n!!!!!!!!!!!!!Ponging'
         self.ircsock.send('PONG tmi.twitch.tv\r\n')
 
-    def print_loop(self):
+    def decompose_message(self, message):
+        print message
+        chat_array = message.split(':', 2)
+        print chat_array
+        
+        #separate out the user, if me, drop entry
+        #also doing bug checking on list index failures
+        try:
+            user = chat_array[1].split('!')[0]
+            if user == 'chatmetrics':
+                return None
+            
+            channel = chat_array[1].split('#')[1].strip(' ')
+            message = chat_array[2]
+            msg_dict = {'user' : user,
+                'channel' : channel,
+                'message' : message}
+            print msg_dict
+            return msg_dict
+        except:
+            return None
+
+    def insert_log(self, entry):
+        chat_id = self.collection.insert_one(entry).inserted_id
+        self.log_count += 1
+        print '!!!!!!LOG COUNT = ' + str(self.log_count)
+
+    def chat_loop(self):
         while 1:
             try:
                 ircmsg = self.ircsock.recv(2048)
-                ircmsg = ircmsg.strip('\n\r')
-                if ircmsg.find('PING tmi.twitch.tv') != -1:
-                    self.pong()
-                print ircmsg
-                time.sleep(0.2)
-            except:
+                ircmsg = ircmsg.split('\r\n')
+                for msg in ircmsg:
+                    if msg.find('PING :tmi.twitch.tv') != -1:
+                        self.pong()
+                    entry = self.decompose_message(msg)
+                    if entry:
+                        self.insert_log(entry)
+                    time.sleep(0.2)
+            except Exception as e:
+                print e
                 time.sleep(0.2)
                 continue
+
 
 if __name__ =='__main__':
     chat = chat_listener()
     chat.login_routine()
-    chat.join_channel('taketv')
-    chat.print_loop()
+    chat.join_channel('wagamamatv')
+    chat.chat_loop()
