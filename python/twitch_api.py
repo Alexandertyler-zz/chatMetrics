22b660d98ad883177d5ff77955436066c65e6c00
Alex Tyler <at@norsecorp.com>
2015-12-25 14:51:05 -0600

    Pprint the json response

--- python/twitch_api.py
+++ python/twitch_api.py
@@ -1,15 +1,20 @@
 import requests
+import pprint
 
 class twitch_api:
 
     def __init__(self):
         #derp do nothing for now
+        self.ops = { 'client_id': 'rk5jc5h3ossghumsqhq6mciwzmyce'}
+   
 
     def get_featured_streams(self, limit=25, offset=0):
         ops = {'limit': limit, 'offset': offset}
+        ops.update(self.ops)
+
         r = requests.get('https://api.twitch.tv/kraken/streams/featured', params=ops)
         print (r.url)
-        print (r.text)
+        pprint.pprint(r.json())
 
 if __name__ == '__main__':
     api = twitch_api()
