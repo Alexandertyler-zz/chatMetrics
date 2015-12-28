6fa2d2f9567a77fb2e8d53aeae24df27788d2a18
Alex Tyler <at@norsecorp.com>
2015-12-27 21:13:23 -0600

    Added in server using express but still debugging the views not serving properly.

---
+++ www/server.js
@@ -0,0 +1,19 @@
+//server
+var express = require('express');
+var app = express();
+//mongodb handler
+var mongoose = require('mongoose');
+//used for logging requests to the console
+var morgan = require('morgan');
+
+//mongoose.connect('mongodb://');
+
+app.use(express.static(__dirname + '/public/static'));
+app.use(morgan('dev'));
+
+app.get('/', function(req, res) {
+  res.sendfile('index.html');
+});
+
+app.listen(8080);
+console.log('App is listening on port 8080');
